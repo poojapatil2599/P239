f08fc57d889404a5ad8afd173789e178470f3d52
poojapatil2599 <125896895+poojapatil2599@users.noreply.github.com>
2023-06-01 15:32:42 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 24 13:01:17 2020
 
 """
 
@@ -8,25 +7,18 @@ Created on Thu Sep 24 13:01:17 2020
 
 import pandas as pd
 import streamlit as st 
-from sklearn.linear_model import LogisticRegression
 from pickle import dump
 from pickle import load
 
-st.title('Model Deployment: Logistic Regression')
+st.title('Model Deployment: Tweeter semantic Analysis')
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    CLMSEX = st.sidebar.selectbox('Gender',('1','0'))
-    CLMINSUR = st.sidebar.selectbox('Insurance',('1','0'))
-    SEATBELT = st.sidebar.selectbox('SeatBelt',('1','0'))
-    CLMAGE = st.sidebar.number_input("Insert the Age")
-    LOSS = st.sidebar.number_input("Insert Loss")
-    data = {'CLMSEX':CLMSEX,
-            'CLMINSUR':CLMINSUR,
-            'SEATBELT':SEATBELT,
-            'CLMAGE':CLMAGE,
-            'LOSS':LOSS}
+    Tweets = st.sidebar.selectbox('Text',('1','0'))
+    Class = st.sidebar.selectbox('Figurative',('1','0'))
+    data = {'Tweets':Tweets,
+            'Class':Class}
     features = pd.DataFrame(data,index = [0])
     return features 
     
@@ -36,7 +28,7 @@ st.write(df)
 
 
 # load the model from disk
-loaded_model = load(open('Logistic_Model.sav', 'rb'))
+loaded_model = load(open('C:/Users/Pooja Patil/Downloads/tweet.csv', 'rb'))
 
 prediction = loaded_model.predict(df)
 prediction_proba = loaded_model.predict_proba(df)
